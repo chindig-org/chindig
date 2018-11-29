d43b01d3bd3096b61afd64268eba6781b947ef76
Chung-An Chiu <bb5868@gmail.com>
2018-11-29 16:01:25 -0600

    added some basic content, really hacky, not clean code

--- src/EventView.js
+++ src/EventView.js
@@ -6,6 +6,16 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBack from '@material-ui/icons/ArrowBack';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import SearchIcon from '@material-ui/icons/Search';
 
 import { Link } from "react-router-dom";
 
@@ -16,6 +26,65 @@ const styles = theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
+  layout: {
+    width: 'auto',    
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    marginTop: theme.spacing.unit *3,
+  },
+  toolbarMain: {
+    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+  },
+  mainFeaturedPost: {
+    // backgroundColor: theme.palette.grey[800],
+    // color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 1,
+    paddingBottom: 0,
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 2}px`,
+    paddingLeft: 0,
+    paddingBottom: 0,
+    [theme.breakpoints.up('md')]: {
+      paddingRight: 0,
+    },
+  },
+  mainGrid: {
+    marginTop: theme.spacing.unit * 1,
+  },
+  sidebarAboutBox: {
+    padding: theme.spacing.unit * 2,
+    backgroundColor: theme.palette.grey[200],
+  },
+  sidebarSection: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  footer: {
+    backgroundColor:  theme.palette.primary.main,
+    fontSize: 25,
+    color: theme.palette.grey[50],
+    marginTop: theme.spacing.unit * 8,
+    padding: `${theme.spacing.unit * 6}px 0`,
+  },
+  slogan: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Great Vibes',
+  },
+  mainImage: {
+    borderRadius: 5,
+  }
 });
 
 class EventView extends React.Component {
@@ -55,16 +124,79 @@ class EventView extends React.Component {
             <Typography variant="h6" color="inherit" className={classes.grow}>
               <div className="logo">Chindig</div>
             </Typography>
-            <span> | </span>
-
-            {eventObj.name}
-
-
           </Toolbar>
         </AppBar>
 
-        {eventObj.namne}
-        {eventObj.url}
+        <span></span>        
+        <React.Fragment>
+      <CssBaseline />
+      <div className={classes.layout}>
+        <main>
+          {/* Main featured post */}
+          <div className={classes.mainFeaturedPost}>
+            <img className={classes.mainImage} src={`./${eventObj.image}`} style={{width:'100%', height:500}}/>
+            <Grid container>
+              <Grid item >
+                <div className={classes.mainFeaturedPostContent}>
+                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                    {eventObj.name}
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    {eventObj.start} @ {eventObj.location} 
+                  </Typography>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+          <Grid container spacing={40} className={classes.mainGrid}>
+            {/* Main content */}
+            <Grid item xs={12} md={8}>
+              <Typography variant="h6" gutterBottom>
+                Detail
+              </Typography>
+              <Divider />
+              <p> 
+                {eventObj.description}
+              </p>
+            </Grid>
+            {/* End main content */}
+            {/* Sidebar */}
+            {/* <Grid item xs={12} md={4}>
+              <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom>
+                  About
+                </Typography>
+                <Typography>
+                  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit
+                  amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+                </Typography>
+              </Paper>
+              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Archives
+              </Typography>
+              {archives.map(archive => (
+                <Typography key={archive}>{archive}</Typography>
+              ))}
+              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Social
+              </Typography>
+              {social.map(network => (
+                <Typography key={network}>{network}</Typography>
+              ))}
+            </Grid> */}
+            {/* End sidebar */}
+          </Grid>
+        </main>
+      </div>
+      {/* Footer */}
+      <footer className={classes.footer}>        
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+          <div className="logo">Chindig</div>
+          <div className={classes.slogan}>Find your community</div>
+        </Typography>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
 
       </div>
     );

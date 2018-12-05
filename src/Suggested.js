95c0743edcb1d7512168a6f26391f1afabc327c4
nconrad <nconrad@anl.gov>
2018-12-04 19:58:00 -0600

    making whole card clickable and styling

--- src/Suggested.js
+++ src/Suggested.js
@@ -3,14 +3,21 @@ import moment from '../node_modules/moment';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
 
 import { Link } from "react-router-dom";
+
+
 
 const styles = theme => ({
   input: {
     display: 'none',
   },
+  card: {
+    padding: '0px'
+  }
 });
 
 
@@ -37,43 +44,51 @@ class Suggested extends Component {
   }
 
   onEventSave = (evt, eventObj) => {
+    evt.preventDefault();
     this.props.onSave(eventObj);
   }
 
   onEventUnsave = (evt, eventObj) => {
+    evt.preventDefault();
     this.props.onUnsave(eventObj);
   }
 
   render() {
+    const { classes } = this.props;
+
     let events = this.state.events.map((event, i) =>
       <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={i} >
-        <div className="event">
-          <img src={`./${event.image}`} alt="test" />
+        <Link to={'/event/' + event.name}>
+          <Card className={`${classes.card} event`}>
 
-          <div className="event-tag">
-            <Chip label={event.tag} color="secondary" />
-          </div>
+            <img src={`./${event.image}`} alt="test" />
 
-          <div className="event-text">
-            <Link className="name" to={'/event/' + event.name}>{event.name}</Link>
-            <div className="location">@ {event.location}</div>
+            <div className="event-tag">
+              <Chip label={event.tag} color="secondary" />
+            </div>
 
-            <div className="day">{this.getDate(event.start)}</div>
-            <div className="time">{this.getTime(event.start)}</div>
-          </div>
+            <div className="event-text">
+              <div className="name">{event.name}</div>
+              <div className="location">@ {event.location}</div>
 
-          <div className="event-save">
-            {!event.saved &&
-              <Button variant="contained" color="primary" className={styles.button} onClick={ (e) => this.onEventSave(e, event)}>
-                Save
-              </Button>}
+              <div className="day">{this.getDate(event.start)}</div>
+              <div className="time">{this.getTime(event.start)}</div>
+            </div>
 
-            {event.saved &&
-              <Button variant="contained" color="secondary" className={styles.button} onClick={ (e) => this.onEventUnsave(e, event)}>
-                Unsave
-              </Button>}
-          </div>
-        </div>
+            <div className="event-save">
+              {!event.saved &&
+                <Button variant="contained" color="primary" className={styles.button} onClick={ (e) => this.onEventSave(e, event)}>
+                  Save
+                </Button>}
+
+              {event.saved &&
+                <Button variant="contained" color="secondary" className={styles.button} onClick={ (e) => this.onEventUnsave(e, event)}>
+                  Unsave
+                </Button>}
+            </div>
+
+          </Card>
+        </Link>
       </Grid>
     )
 
